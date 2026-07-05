b893ccfa3f1f16a44b996d9c99becee0eadae3d0
MGMPKING <100523145+MGMPKING@users.noreply.github.com>
2026-07-05 16:40:07 +0100

    Update bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "LuxanaMC's official ticket bot! play.luxanamc.xyz",
+        name: "LuxanaMC 👀",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 3,
       },
     ],
   },
